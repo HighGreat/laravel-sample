c39a9a91caaa2e03c5a2c89dffcbb67ae56b09bd
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-06-20 23:52:12 +0900

    task一覧表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Folder;
+use App\Task;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
         $folders = Folder::all();
+        $current_folder = Folder::find($id);
+        $tasks = $current_folder->tasks()->get();
         return view('tasks.index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
             ]);
     }
 }
