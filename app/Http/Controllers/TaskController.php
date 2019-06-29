4e0b720ee746532fbd9f0b6e2f9b74bff8682efd
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-06-29 18:11:10 +0900

    タスク作成機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTask;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
@@ -18,5 +19,30 @@ class TaskController extends Controller
             'current_folder_id' => $id,
             'tasks' => $tasks,
             ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks.create', [
+          'folder_id' => $id,
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+      $current_folder = Folder::find($id);
+      
+      $task = new Task();
+      $task->title = $request->title;
+      $task->due_date = $request->due_date;
+
+      $current_folder->tasks()->save($task);
+      
+      return redirect()->route('tasks.index', [
+        'id' => $current_folder->id,
+      ]);
     }
 }
