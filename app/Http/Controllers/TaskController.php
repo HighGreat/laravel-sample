cf7eb51e1ff6092cef9035620cc0479397d9b780
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-06-30 16:25:58 +0900

    タスクの編集機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
@@ -44,5 +45,34 @@ class TaskController extends Controller
       return redirect()->route('tasks.index', [
         'id' => $current_folder->id,
       ]);
+    }
+
+    /*
+     * GET /folders/{id}/tasks/{tasks_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+      $task = Task::find($task_id);
+
+      return view('tasks.edit', [
+        'task' => $task,
+      ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+      // 1
+      $task = Task::find($task_id);
+
+      // 2
+      $task->tatile = $request->title;
+      $task->status = $request->status;
+      $task->due_date = $request->due_date;
+      $task->save();
+
+      // 3
+      return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
+       ]);
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use Carbon\Carbon;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 
 class TaskTest extends TestCase
 {
@@ -71,5 +72,42 @@ class TaskTest extends TestCase
       $response->assertSessionHasErrors([
         'due_date' => '期限日 には今日以降の日付を入力してください。',
       ]);
+    }
+
+    /**
+     * 状態がからの場合エラー
+     * @test
+     */
+    public function status_should_not_be_empty()
+    {
+      $this->seed('TasksTableSeeder');
+
+      $response = $this->post('/folders/1/tasks/1/edit', [
+        'title' => 'Sample Task',
+        'due_date' => Carbon::today()->format('Y/m/d'),
+        'status' => '',
+      ]);
+
+      $response->assertSessionHasErrors([
+        'status' => '状態 は必須入力です。',
+      ]);
+    }
+
+    /**
+     * 状態が定義された値出ない場合はバリデーションエラー
+     * @test
+     */
+    public function status_should_be_within_defined_numbers()
+    {
+      $this->seed('TasksTableSeeder');
+
+      $response = $this->post('/folders/1/tasks/1/edit', [
+        'title' => 'Sample task',
+        'due_date' => Carbon::today()->format('Y/m/d'),
+        'status' => 999,
+      ]);
+
+      $response->assertSessionHasErrors([
+        'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。']);
     }
 }
