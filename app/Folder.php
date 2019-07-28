1e2673b6588d1f467073c380c73aa80b0526037f
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-07-28 15:07:40 +0900

    error handling

--- app/Folder.php
+++ app/Folder.php
@@ -4,10 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class folder extends Model
+class Folder extends Model
 {
     public function tasks()
     {
-        return $this->hasMany('App\task');
+        return $this->hasMany('App\Task');
     }
 }
